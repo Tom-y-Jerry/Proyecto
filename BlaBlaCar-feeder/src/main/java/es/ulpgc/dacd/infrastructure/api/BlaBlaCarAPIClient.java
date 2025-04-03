ef94485e2d6b235edac3bda98adb71489f00d9d2
carlota <a@a.com>
2025-04-03 16:27:57 +0100

    change the structure

---
+++ BlaBlaCar-feeder/src/main/java/es/ulpgc/dacd/infrastructure/api/BlaBlaCarAPIClient.java
@@ -0,0 +1,31 @@
+package es.ulpgc.dacd.infrastructure.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class BlaBlaCarAPIClient {
+    private static final String API_URL = "https://bus-api.blablacar.com/v3/stops";
+    private final String apiKey;
+    private final OkHttpClient client = new OkHttpClient();
+
+    public BlaBlaCarAPIClient(String apiKey) {
+        // Ruta externa al archivo que contiene la API key
+        this.apiKey = ApiKeyLoader.loadApiKey("C:\\Users\\carlo\\Desktop\\Proyecto\\apikeyblablacar.txt");
+    }
+
+    public String fetchStopsJson() throws Exception {
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .addHeader("Authorization", "Token " + apiKey)
+                .addHeader("apikeyblablacar.txt" , apiKey)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new Exception("API error: " + response);
+            }
+            return response.body().string();
+        }
+    }
+}
