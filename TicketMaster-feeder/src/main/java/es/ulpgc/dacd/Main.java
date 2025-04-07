9e1e23cdcf2eba8eba05bbbb944142636e34c2c0
l-cruz <luciaacruztoste@gmail.com>
2025-04-07 19:36:52 +0100

    Main created

--- TicketMaster-feeder/src/main/java/es/ulpgc/dacd/Main.java
+++ TicketMaster-feeder/src/main/java/es/ulpgc/dacd/Main.java
@@ -1,17 +1,27 @@
 package es.ulpgc.dacd;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import es.ulpgc.dacd.controller.TicketMasterServiceController;
+import es.ulpgc.dacd.domain.port.Events;
+import es.ulpgc.dacd.domain.port.EventsRepository;
+import es.ulpgc.dacd.infrastructure.api.ApiKeyLoader;
+import es.ulpgc.dacd.infrastructure.api.TicketMasterAPIClient;
+import es.ulpgc.dacd.infrastructure.api.TicketMasterEvents;
+import es.ulpgc.dacd.infrastructure.adapter.SQLiteEventsRepository;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    private static final String DB_URL = "jdbc:sqlite:data.db";
+
+    public static void main(String[] args) {
+        TicketMasterAPIClient apiClient = new TicketMasterAPIClient();
+        Events events = new TicketMasterEvents(apiClient);
+        EventsRepository repository = new SQLiteEventsRepository(DB_URL);
+        TicketMasterServiceController service = new TicketMasterServiceController(events, repository);
+
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(service::run, 0, 1, TimeUnit.HOURS);
+
     }
 }
