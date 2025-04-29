9134c3520a4b7a5b43cc7c762c145fa7b493c13f
l-cruz <luciaacruztoste@gmail.com>
2025-04-29 12:46:21 +0100

    created new class Trip

---
+++ blablacar-feeder/src/main/java/es/ulpgc/dacd/domain/model/Trip.java
@@ -0,0 +1,29 @@
+package es.ulpgc.dacd.domain.model;
+
+import java.util.List;
+
+public class Trip {
+    private final String origin;
+    private final String destination;
+    private final String departure;
+    private final String arrival;
+    private final int priceCents;
+    private final String currency;
+
+    public Trip(String origin, String destination, String departure, String arrival, int priceCents, String currency) {
+        this.origin = origin;
+        this.destination = destination;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.priceCents = priceCents;
+        this.currency = currency;
+    }
+
+    public String getOrigin() {return origin;}
+    public String getDestination() {return destination;}
+    public String getDeparture() {return departure;}
+    public String getArrival() {return arrival;}
+    public int getPriceCents() {return priceCents;}
+    public String getCurrency() {return currency;}
+
+}
