cf9022a9f0a1a384435db82f7da3457dd9890c6c
l-cruz <luciaacruztoste@gmail.com>
2025-04-29 12:47:08 +0100

    modified Main

--- blablacar-feeder/src/main/java/es/ulpgc/dacd/entrypoints/scheduler/SchedulerMain.java
+++ blablacar-feeder/src/main/java/es/ulpgc/dacd/entrypoints/scheduler/SchedulerMain.java
@@ -2,12 +2,18 @@ package es.ulpgc.dacd.entrypoints.scheduler;
 
 import es.ulpgc.dacd.application.service.StationService;
 import es.ulpgc.dacd.application.service.FareService;
+import es.ulpgc.dacd.application.service.TripService;
+import es.ulpgc.dacd.domain.port.Trips;
+import es.ulpgc.dacd.domain.port.TripsRepository;
 import es.ulpgc.dacd.infrastructure.api.BlaBlaCarAPIClient;
 import es.ulpgc.dacd.infrastructure.api.BlaBlaCarBusAPIClient;
 import es.ulpgc.dacd.infrastructure.api.BlaBlaCarBusFares;
 import es.ulpgc.dacd.infrastructure.api.BlaBlaCarStations;
+import es.ulpgc.dacd.infrastructure.api.BlaBlaCarTrips;
 import es.ulpgc.dacd.infrastructure.persistence.SQLiteStationsRepository;
 import es.ulpgc.dacd.infrastructure.persistence.SQLiteFaresRepository;
+import es.ulpgc.dacd.infrastructure.persistence.SQLiteTripsRepository;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -18,7 +24,9 @@ public class SchedulerMain {
     public static void main(String[] args) {
         StationService stationService = createStationService();
         FareService fareService = createFareService();
-        startScheduler(stationService, fareService);
+        TripService tripService = createTripService();
+
+        startScheduler(stationService, fareService, tripService);
     }
 
     private static StationService createStationService() {
@@ -35,10 +43,21 @@ public class SchedulerMain {
         return new FareService(fares, repository);
     }
 
-    private static void startScheduler(StationService stationService, FareService fareService) {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
-        scheduler.scheduleAtFixedRate(stationService::run, 0, 1, TimeUnit.HOURS);
-        scheduler.scheduleAtFixedRate(fareService::run, 0, 4, TimeUnit.HOURS);
+    private static TripService createTripService() {
+        Trips trips = new BlaBlaCarTrips(DB_URL);
+        TripsRepository repository = new SQLiteTripsRepository(DB_URL);
+        return new TripService(trips, repository);
+    }
+
+    private static void startScheduler(StationService stationService, FareService fareService, TripService tripService) {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(() -> {
+            stationService.run();   // 1. Guarda estaciones
+            fareService.run();      // 2. Guarda fares
+            tripService.run();      // 3. Guarda trips con nombres
+        }, 0, 2, TimeUnit.HOURS);
     }
 }
+
+
 
