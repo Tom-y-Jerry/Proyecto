a2c870a87ababfcc6c8d137c11ad1e3e621d8e5d
carlo <a@a.com>
2025-04-29 21:48:03 +0100

    rename Main classes

---
+++ ticketmaster-feeder/src/main/java/es/ulpgc/dacd/entrypoints/publisher/PublisherMain.java
@@ -0,0 +1,19 @@
+package es.ulpgc.dacd.entrypoints.publisher;
+
+import es.ulpgc.dacd.application.publisher.EventPublisher;
+import es.ulpgc.dacd.infrastructure.api.*;
+import es.ulpgc.dacd.infrastructure.broker.ActiveMQPublisher;
+public final class PublisherMain {
+
+    public static void main(String[] args) {
+        try (ActiveMQPublisher mq = new ActiveMQPublisher("tcp://localhost:61616")) {
+
+            new EventPublisher(
+                    new TicketMasterEvents(new TicketMasterAPIClient()),
+                    mq).run();
+
+        } catch (Exception e) {
+            System.err.println("❌ PublisherMain: " + e.getMessage());
+        }
+    }
+}
