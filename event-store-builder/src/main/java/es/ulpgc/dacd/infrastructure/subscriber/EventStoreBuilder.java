555b2d75bb9cb6c421c15952d25028a140acf7a6
carlo <a@a.com>
2025-05-01 13:55:51 +0100

    Serialize Trip as JSON using custom Instant serializer

--- event-store-builder/src/main/java/es/ulpgc/dacd/infrastructure/subscriber/EventStoreBuilder.java
+++ event-store-builder/src/main/java/es/ulpgc/dacd/infrastructure/subscriber/EventStoreBuilder.java
@@ -2,8 +2,13 @@ package es.ulpgc.dacd.infrastructure.subscriber;
 
 public class EventStoreBuilder {
     public static void main(String[] args) {
+        String[] topics = (args.length > 0)
+                ? args
+                : new String[] { "prediction.Trips", "prediction.Events" };
+
         EventConsumer consumer = new EventConsumer("tcp://localhost:61616");
-        consumer.subscribe("prediction.Events");
-        consumer.subscribe("prediction.Stations");
+        for (String topic : topics) {
+            consumer.subscribe(topic);
+        }
     }
 }
