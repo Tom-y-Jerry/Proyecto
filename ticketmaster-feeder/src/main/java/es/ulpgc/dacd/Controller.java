246ca9857ac57d0aab49ee6d0e02af693ccc948f
l-cruz <luciaacruztoste@gmail.com>
2025-05-04 23:31:11 +0100

    Created Controller to periodically fetch and store events

---
+++ ticketmaster-feeder/src/main/java/es/ulpgc/dacd/Controller.java
@@ -0,0 +1,41 @@
+package es.ulpgc.dacd;
+
+import es.ulpgc.dacd.domain.Event;
+import es.ulpgc.dacd.ports.EventProvider;
+import es.ulpgc.dacd.ports.EventStorage;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Controller {
+    private final EventProvider eventProvider;
+    private final EventStorage storage;
+
+    public Controller(EventProvider eventProvider, EventStorage storage) {
+        this.eventProvider = eventProvider;
+        this.storage = storage;
+    }
+
+    public void execute() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            System.out.println("Ejecutando consulta de eventos...");
+            List<Event> events = eventProvider.provide();
+            for (Event event : events) {
+                storage.save(event);
+                System.out.printf(formatEvent(event));
+            }
+        }, 0, 1, TimeUnit.HOURS);
+    }
+
+    private String formatEvent(Event event) {
+        return String.format("Evento: %s | %s | %s | %s\n",
+                event.getId(),
+                event.getName(),
+                event.getDate(),
+                event.getCity());
+    }
+}
