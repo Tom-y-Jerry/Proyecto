e33371289ebd0c2ddaa91cbde6b63580f6f25e1b
l-cruz <luciaacruztoste@gmail.com>
2025-05-04 23:31:31 +0100

    Created Main class to initialize and run the event controller

---
+++ ticketmaster-feeder/src/main/java/es/ulpgc/dacd/Main.java
@@ -0,0 +1,21 @@
+package es.ulpgc.dacd;
+
+import es.ulpgc.dacd.adapters.TicketMasterEventProvider;
+import es.ulpgc.dacd.adapters.ActiveMQEventStorage;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Argumentos requeridos: <eventsUrl> <apiKey>");
+            return;
+        }
+
+        String eventsUrl = args[0];
+        String apiKey = args[1];
+
+        var provider = new TicketMasterEventProvider(eventsUrl, apiKey);
+        var storage = new ActiveMQEventStorage();
+
+        new Controller(provider, storage).execute();
+    }
+}
