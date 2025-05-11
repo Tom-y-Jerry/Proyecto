e154809a449aff78b2bd33b1bd1b466f83965288
carlo <a@a.com>
2025-05-11 23:30:45 +0100

    clean up ProcessTicketmasterEvent with focused logic

--- bussines-unit/src/main/java/es/ulpgc/dacd/business/application/processor/ProcessTicketmasterEvent.java
+++ bussines-unit/src/main/java/es/ulpgc/dacd/business/application/processor/ProcessTicketmasterEvent.java
@@ -1,4 +1,5 @@
 package es.ulpgc.dacd.business.application.processor;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import es.ulpgc.dacd.business.application.service.DatamartService;
@@ -12,16 +13,17 @@ public class ProcessTicketmasterEvent implements EventProcessor {
 
     @Override
     public void process(String rawEvent) {
-        JsonObject json = JsonParser.parseString(rawEvent).getAsJsonObject();
         try {
-            String id = json.get("id").getAsString();
-            String name = json.get("name").getAsString();
-            String date = json.get("date").getAsString();
-            String time = json.get("time").getAsString();
-            String city = json.get("city").getAsString();
-            String ss = json.get("ss").getAsString();
-
-            datamart.insertEvent(id, name, date, time, city, ss, rawEvent);
+            JsonObject json = JsonParser.parseString(rawEvent).getAsJsonObject();
+            datamart.insertEvent(
+                    json.get("id").getAsString(),
+                    json.get("name").getAsString(),
+                    json.get("date").getAsString(),
+                    json.get("time").getAsString(),
+                    json.get("city").getAsString(),
+                    json.get("ss").getAsString(),
+                    rawEvent
+            );
         } catch (Exception e) {
             throw new RuntimeException("Error processing Ticketmaster event", e);
         }
