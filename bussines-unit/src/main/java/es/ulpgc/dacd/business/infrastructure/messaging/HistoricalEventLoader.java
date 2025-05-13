962310eca36ac7bb0de59c62ccb81cb7292b9813
carlo <a@a.com>
2025-05-13 14:36:55 +0100

    make HistoricalEventLoader generic and support Instant deserialization

--- bussines-unit/src/main/java/es/ulpgc/dacd/business/infrastructure/messaging/HistoricalEventLoader.java
+++ bussines-unit/src/main/java/es/ulpgc/dacd/business/infrastructure/messaging/HistoricalEventLoader.java
@@ -1,13 +1,23 @@
 package es.ulpgc.dacd.business.infrastructure.messaging;
 
+import com.google.gson.*;
 import es.ulpgc.dacd.business.application.processor.EventProcessor;
+
 import java.io.*;
+import java.lang.reflect.Type;
+import java.time.Instant;
 
-public class HistoricalEventLoader {
-    private final EventProcessor processor;
+public class HistoricalEventLoader<T> {
+    private final EventProcessor<T> processor;
+    private final Class<T> type;
+    private final Gson gson;
 
-    public HistoricalEventLoader(EventProcessor processor) {
+    public HistoricalEventLoader(EventProcessor<T> processor, Class<T> type) {
         this.processor = processor;
+        this.type = type;
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(Instant.class, new InstantDeserializer())
+                .create();
     }
 
     public void loadFromDirectory(String directoryPath) {
@@ -29,10 +39,21 @@ public class HistoricalEventLoader {
     private void loadFile(File file) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-            while ((line = reader.readLine()) != null) processor.process(line);
+            while ((line = reader.readLine()) != null) {
+                T item = gson.fromJson(line, type);
+                processor.process(item);
+            }
             System.out.println("Cargado archivo histórico: " + file.getName());
         } catch (Exception e) {
             System.err.println("Error leyendo archivo: " + file.getAbsolutePath());
+        }
+    }
+
+    private static class InstantDeserializer implements JsonDeserializer<Instant> {
+        @Override
+        public Instant deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            return Instant.parse(json.getAsString());
         }
     }
 }
