1f9e8f6d6d7c6c2bf76f39bf6e0eef6f67ffe307
carlo <a@a.com>
2025-05-13 17:06:28 +0100

    simplify Main by removing originId argument and using updated provider

--- blablacar-feeder/src/main/java/es/ulpgc/dacd/Main.java
+++ blablacar-feeder/src/main/java/es/ulpgc/dacd/Main.java
@@ -1,22 +1,19 @@
 package es.ulpgc.dacd;
-
-
 import es.ulpgc.dacd.infrastructure.adapters.ActiveMqTripStorage;
 import es.ulpgc.dacd.infrastructure.adapters.BlablacarTripProvider;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 4) {
-            System.out.println("You need to pass: <stopsUrl> <faresUrl> <apiKey> <originId>");
+        if (args.length < 3) {
+            System.out.println("You need to pass: <stopsUrl> <faresUrl> <apiKey>");
             return;
         }
 
         String stopsUrl = args[0];
         String faresUrl = args[1];
         String apiKey = args[2];
-        int originId = Integer.parseInt(args[3]);
 
-        var provider = new BlablacarTripProvider(stopsUrl, faresUrl, apiKey, originId);
+        var provider = new BlablacarTripProvider(stopsUrl, faresUrl, apiKey);
         var storage = new ActiveMqTripStorage();
         new Controller(provider, storage).execute();
 
