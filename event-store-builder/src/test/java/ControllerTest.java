94339509a72df6323eb3aaadec508d4c33c075a2
l-cruz <luciaacruztoste@gmail.com>
2025-05-17 12:43:06 +0100

    Adapt ControllerTest to new Controller structure

--- event-store-builder/src/test/java/ControllerTest.java
+++ event-store-builder/src/test/java/ControllerTest.java
@@ -1,19 +1,16 @@
 import es.ulpgc.dacd.eventstorebuilder.Controller;
-import es.ulpgc.dacd.eventstorebuilder.adapters.EventListener;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
 
 class ControllerTest {
 
     @Test
-    void shouldSubscribeToAllTopics() {
-        EventListener listener = mock(EventListener.class);
-        Controller controller = new Controller(listener);
+    void shouldSubscribeToTopicsCorrectly() {
+        String brokerUrl = "tcp://localhost:61616";
+        String clientId = "test-client";
+        String[] topics = {"test-topic1", "test-topic2"};
 
-        controller.subscribeToTopics("topic1", "topic2");
-
-        verify(listener).subscribe("topic1");
-        verify(listener).subscribe("topic2");
+        Controller controller = new Controller();
+        controller.start(brokerUrl, clientId, topics);
     }
 }
